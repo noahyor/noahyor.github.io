7e9e84f9d687643b06c6b8f3d7a292061e632096
Noah Yorgey <noah.yorgey@gmail.com>
2024-05-26 15:36:55 -0500

    Done?

--- data/install.ps1
+++ data/install.ps1
@@ -1,4 +1,6 @@
-Invoke-WebRequest https://noahyor.github.io/data/mods -OutFile .\data\mods
-Invoke-WebRequest https://noahyor.github.io/data/scripts -OutFile .\data\scripts
+Invoke-WebRequest https://noahyor.github.io/data/mods.zip -OutFile .\data\mods.zip
+Invoke-WebRequest https://noahyor.github.io/data/scripts.zip -OutFile .\data\scripts.zip
+Expand-Archive .\data\mods.zip .\data\mods\
+Expand-Archive .\data\scripts.zip .\data\scripts\
 Copy-Item .\data\scripts\setup.ps1 .\setup.ps1
 .\setup.ps1
