2222231fb0eb3faa5657893b256def7eaf560efa
Noah Yorgey <noah.yorgey@gmail.com>
2024-05-28 06:21:13 -0500

    Fix THE THINGS
    Also changes the command.

--- data/install.ps1
+++ data/install.ps1
@@ -1,6 +1,12 @@
+New-Item .\data -ItemType Directory
+Write-Output "Downloading Scripts..."
 Invoke-WebRequest https://noahyor.github.io/data/mods.zip -OutFile .\data\mods.zip
+Write-Output "Done!"
+Write-Output "Downloading mods, this may take some time..."
 Invoke-WebRequest https://noahyor.github.io/data/scripts.zip -OutFile .\data\scripts.zip
-Expand-Archive .\data\mods.zip .\data\mods\
-Expand-Archive .\data\scripts.zip .\data\scripts\
+Write-Output "Done!"
+Expand-Archive .\data\mods.zip .\data\
+Expand-Archive .\data\scripts.zip .\data\
 Copy-Item .\data\scripts\setup.ps1 .\setup.ps1
 .\setup.ps1
+Remove-Item .\install.ps1
