97c1b5342063cd684f101174bdfc6bf18db22d9b
Noah Yorgey <noah.yorgey@gmail.com>
2024-05-29 09:34:25 -0500

    Add Versioning

--- data/install.ps1
+++ data/install.ps1
@@ -1,18 +1,39 @@
+Clear-Host
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
 if (!(Test-Path .\data\)) {
     $null = New-Item .\data -ItemType Directory
 }
+if (Test-Path .\data\VERSION) {
+    Write-Host "Checking version..."
+    Invoke-WebRequest https://noahyor.github.io/data/VERSION -OutFile .\data\VERSIONCHECK
+    $LV = Get-Content .\data\VERSION
+    $RV = Get-Content .\data\VERSIONCHECK
+    if ($LV == "DEV") {
+        Write-Host "Done! Status: AHEAD; Version: DEV"
+    } elseif ($LV == $RV) {
+        Write-Host "Done! Status: UP-TO-DATE; Version: {}" $LV
+    } else {
+        Write-Host "Done! Status: BEHIND; Version: {} -> {}" $LV $RV
+        Write-Host "Updating..."
+        Remove-Item .\data\mods\
+    }
+}
 if (!(Test-Path .\data\mods\)) {
-    Write-Output "Downloading mods, this will take a few minutes..."
+    Write-Host "Downloading mods, this will take a few minutes..."
     Invoke-WebRequest https://noahyor.github.io/data/mods.zip -OutFile .\data\mods.zip
-    Write-Output "Done!"
+    Write-Host "Done!"
     Expand-Archive .\data\mods.zip .\data\
     Remove-Item .\data\mods.zip
 }
 
 if (!(Test-Path .\data\scripts\)) {
-    Write-Output "Downloading Scripts..."
+    Write-Host "Downloading Scripts..."
     Invoke-WebRequest https://noahyor.github.io/data/scripts.zip -OutFile .\data\scripts.zip
-    Write-Output "Done!"
+    Write-Host "Done!"
     Expand-Archive .\data\scripts.zip .\data\
     Remove-Item .\data\scripts.zip
 }
@@ -20,4 +41,7 @@ if (!(Test-Path .\setup.ps1)) {
     Copy-Item .\data\scripts\setup.ps1 .\setup.ps1
 }
 .\setup.ps1
+if (!(Test-Path .\data\VERSION)) {
+    Invoke-WebRequest https://noahyor.github.io/data/VERSION -OutFile .\data\VERSION
+}
 Remove-Item .\install.ps1
