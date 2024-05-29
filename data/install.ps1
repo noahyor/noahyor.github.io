7859d87a5673cb477792d5f3cf9c3b8dd39f8cef
Noah Yorgey <noah.yorgey@gmail.com>
2024-05-28 19:47:33 -0500

    Switch order

--- data/install.ps1
+++ data/install.ps1
@@ -1,8 +1,8 @@
 New-Item .\data -ItemType Directory
-Write-Output "Downloading Scripts..."
+Write-Output "Downloading mods, this may take some time..."
 Invoke-WebRequest https://noahyor.github.io/data/mods.zip -OutFile .\data\mods.zip
 Write-Output "Done!"
-Write-Output "Downloading mods, this may take some time..."
+Write-Output "Downloading Scripts..."
 Invoke-WebRequest https://noahyor.github.io/data/scripts.zip -OutFile .\data\scripts.zip
 Write-Output "Done!"
 Expand-Archive .\data\mods.zip .\data\
