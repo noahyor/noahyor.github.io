e8bda37fb74a3395023e2e6c7bbb16865250c38b
Noah Yorgey <noah.yorgey@gmail.com>
2024-07-01 10:07:37 -0500

    Lots of changes

--- data/CCraft.ps1
+++ data/CCraft.ps1
@@ -15,7 +15,7 @@ if (!(Test-Path .\data\)) {
 }
 
 # Version Checking
-# Version Format: CCraft-(Major).(Minor).(Doc)-(What Changed: DOC/MOD/SHD)
+# Version Format: CCraft-(Major).(Minor).(Doc)-(What Changed: DOC/MOD/SHD/SCR)
 if (Test-Path ~\AppData\Roaming\.minecraft\.CCraft\VERSION.txt) {
     Write-Host "Checking version..."
     Invoke-WebRequest https://noahyor.github.io/data/VERSION.txt -OutFile ~\AppData\Roaming\.minecraft\.CCraft\VERSIONCHECK.txt
@@ -41,7 +41,7 @@ if (Test-Path ~\AppData\Roaming\.minecraft\.CCraft\VERSION.txt) {
             }
             
             # Game files changed, mark for update (remove the respective directories)
-            if (($RV -ccontains "MOD") -or ($RV -ccontains "SHD")) {
+            if (($RV -ccontains "MOD") -or ($RV -ccontains "SHD") -or ($RV -ccontains "SCR")) {
                 Write-Host "Done! Status: BEHIND; Version: {0} -> {1}" -f $LV $RV
                 Write-Host "Updating..."
                 if ($RV -ccontains "MOD") {
@@ -49,6 +49,9 @@ if (Test-Path ~\AppData\Roaming\.minecraft\.CCraft\VERSION.txt) {
                 }
                 if ($RV -ccontains "SHD") {
                     Remove-Item .\data\shaderpacks\
+                }
+                if ($RV -ccontains "SCR") {
+                    Remove-Item .\data\scripts\
                 }
             }
         }
@@ -62,6 +65,7 @@ if (Test-Path ~\AppData\Roaming\.minecraft\.CCraft\VERSION.txt) {
 if (!(Test-Path .\data\mods\)) {
     Write-Host "Please do not cancel while download is in progress."
     Write-Host "Downloading mods, this will take a few minutes..."
+    Write-Host "Feel free to grab a snack at this time."
     Invoke-WebRequest https://noahyor.github.io/data/mods.zip -OutFile .\data\mods.zip
     Write-Host "Decompressing..."
     Expand-Archive .\data\mods.zip .\data\
@@ -97,9 +101,43 @@ if (!(Test-Path .\setup.ps1)) {
     Copy-Item .\data\scripts\setup.ps1 .\setup.ps1
 }
 
-# Run the install script if not already installed.
+# Install CCraft if not already installed.
 if (!(Test-Path ~\AppData\Roaming\.minecraft\.CCraft\)) {
-    .\setup.ps1
+    Write-Host "Downloading NeoForge Installer..."
+    Invoke-WebRequest https://maven.neoforged.net/releases/net/neoforged/forge/1.20.1-47.1.106/forge-1.20.1-47.1.106-installer.jar -OutFile ".\forge-1.20.1-47.1.106-installer.jar"
+    Write-Host "Done!"
+    Write-Host "Installing Forge..."
+    Write-Host "Please press 'Proceed' on the next screen."
+    pause
+    java -jar .\forge-1.20.1-47.1.106-installer.jar
+    Write-Host "Moving files around..."
+    Start-Sleep 2
+    Remove-Item .\forge-1.20.1-47.1.106-installer.jar
+    $null = New-Item ~\AppData\Roaming\.minecraft\.CCraft -ItemType Directory
+    if (Test-Path ~\AppData\Roaming\.minecraft\mods\*.jar) {
+        Write-Host "Previously used mods have been detected."
+        if (!(Test-Path ~\AppData\Roaming\.minecraft\.CCraft\mods_old)) {
+            $null = New-Item ~\AppData\Roaming\.minecraft\.CCraft\mods_old -ItemType Directory
+        }
+        Move-Item "~\AppData\Roaming\.minecraft\mods\*.jar" "~\AppData\Roaming\.minecraft\.CCraft\mods_old\"
+        Write-Host "Other mods have been moved to ~\AppData\Roaming\.minecraft\.CCraft\mods_old\ ."
+        Write-Host "To restore them while not playing CCraft, use restore_other_mods.ps1"
+    } elseif (!(Test-Path ~\AppData\Roaming\.minecraft\mods\)) {
+        $null = New-Item ~\AppData\Roaming\.minecraft\mods\ -ItemType Directory
+    }
+    Copy-Item ".\data\mods\*.jar" "~\AppData\Roaming\.minecraft\mods\"
+    if (!(Test-Path ~\AppData\Roaming\.minecraft\shaderpacks\)) {
+        $null = New-Item ~\AppData\Roaming\.minecraft\shaderpacks\ -ItemType Directory
+    }
+    Copy-Item ".\data\shaderpacks\*.zip" "~\AppData\Roaming\.minecraft\shaderpacks\"
+    Write-Host "Setup is now complete."
+    Write-Warning "However, it is highly recomended to give Minecraft 4 Gigabytes of RAM."
+    Write-Host "To do so, open the Installations tab in the launcher and select Forge."
+    Write-Host "Then, open the 'More Options' dropdown and change the part near the begining from -Xmx2G to -Xmx4G ."
+    Write-Host "To play CCraft, open the Minecraft Launcher, then in the dropdown next to the Play button, select Forge."
+    Write-Host "It will take a minute or two to load. Please be patient."
+    pause
+    Copy-Item .\data\scripts\restore_other_mods.ps1 .\
 }
 
 # Set the current version
